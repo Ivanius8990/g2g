ab86218fc32017509b829790b948c2de0d65ba10
MrSmith <Ivanius8990@users.noreply.github.com>
2022-08-01 16:22:35 +0800

    dsgv

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import time
 
-
-url = 'https://www.g2g.com/categories/wow-gold?region_id=dfced32f-2f0a-4df5-a218-1e068cfadffa'
-options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
-options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
-driver = webdriver.Firefox(executable_path=r'C:\webdriver\geckodriver.exe',options=options)
+s=Service('C:\webdriver\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+url='https://www.g2g.com/categories/wow-gold'
 driver.get(url)
 html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-reg=soup.find('body', class_='desktop no-touch body--light q-body--dialog q-body--prevent-scroll')
-print(soup)
+
+button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[1]/div[5]/div[1]/div/div/div/div[1]/div[3]/div[1]/div/div[3]/div[1]/div/div/button').click()
+time.sleep(10)
+cl_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[1]/div[5]/div[1]/div/div/div/div[3]/aside/div[1]/div/div[2]/div')
+elems = cl_button.find_elements(By.TAG_NAME,"a")
+for elem in elems:
+    print(elem.get_attribute("href"))
 
 
+driver.close()
+driver.quit()
