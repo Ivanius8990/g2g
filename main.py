7febd80d6376417bd8dea80522050355a60463ad
MrSmith <Ivanius8990@users.noreply.github.com>
2022-08-02 17:12:16 +0800

    все работает. Сохраняет в БД

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+import sqlite3
 
 
+
+conn = sqlite3.connect('servers.db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS servers_WOW(
+   serv_id INT PRIMARY KEY,
+   region TEXT,
+   serv_index TEXT);
+""")
+conn.commit()
 
 s=Service('C:\webdriver\chromedriver.exe')
 driver = webdriver.Chrome(service=s)
@@ -36,20 +44,21 @@ for i in region:
     wait.until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[1]/div/div[1]/div[1]/div[5]/div[1]/div/div/div/div[1]/div[3]/div[1]/div/div[3]/div[1]/div/div/div/div/div/aside/div[1]/div/div[2]/div/div[2]')))
     cl_button2 = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[1]/div[5]/div[1]/div/div/div/div[1]/div[3]/div[1]/div/div[3]/div[1]/div/div/div/div/div/aside/div[1]/div/div[2]/div/div[2]')
 
-
-### находим все чекбоксы, прокручиваем и щелкаем по ним
+    ### находим все чекбоксы, прокручиваем и щелкаем по ним
     elems2 = cl_button2.find_elements(By.CLASS_NAME,"text-body2")
-    for i,elem in enumerate(elems2):
+    for elem in elems2:
         driver.execute_script("arguments[0].scrollIntoView();", elem)
         elem.click()
-        servers.append(elem.text)
-
-
-        print(elem.text)
-    print(i+1)
-
-
-
+    driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div[1]/div[5]/div[1]/div/div/div/div[1]/div[3]/div[1]/div/div[3]/div[1]/div/div/div/div/div/aside/div[1]/div/div[3]/div/div[2]/button').click()
+    cur_url=str(driver.current_url)
+    cut_url = cur_url[cur_url.find("%3A") + 3:]
+    servers = cut_url.split(",")
+    print(servers)
+    lst=[]
+    for j,serv in enumerate(servers):
+        lst.append((j+1,i,servers[j]))
+    cur.executemany("INSERT INTO servers_WOW VALUES(?, ?, ?);", lst)
+    conn.commit()
 
 driver.close()
 driver.quit()
